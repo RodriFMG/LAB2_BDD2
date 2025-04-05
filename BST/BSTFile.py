ea2586b70989ef46522c49a20cc0c0025de0eee4
RODRIGO <rodrigo.meza@utec.edu.pe>
2025-04-04 22:19:01 -0500

    PUSH

--- BST/BSTFile.py
+++ BST/BSTFile.py
@@ -9,22 +9,27 @@ class BSTFile:
 
     def __init__(self, File):
 
+        # Verificamos si existe el archivo.
         if os.path.exists(File) is False:
             raise ValueError(f"El fichero de la ruta {File} no existe.")
 
         self.FileName = File
 
+        # Extraemos el nodo padre.
         with open(self.FileName, "r+b") as file:
             file.seek(0, 0)
             bytes_read = file.read(4)
 
+            # Si aún no inserto ningun elemento, no existirá esa cadena de bytes.
             if not bytes_read:
                 file.seek(0, 0)
                 file.write(struct.pack("i", -1))
 
-    # falta no considerar a los elementos BORRADOS.
+    # # O(log2(n)) + O(log2(n)), O(n) + O(n) en el peor de los casos.
     def Insert(self, Venta):
 
+        # Verificamos si existe un registro con el mismo ID.
+        # O(log2(n)), O(n) en el peor de los casos.
         if self.Search(Venta.id) is True:
             raise ValueError(f"El ID: {Venta.id} ya existe entre los registros.")
 
@@ -34,29 +39,37 @@ class BSTFile:
 
             idInsert = Venta.id
             file.seek(0, 2)
+
+            # Insertamos al final el nuevo registro.
             file.write(Venta.to_byte())
             file.seek(0, 0)
 
-            # NO CONSIDERES A LOS ELEMENTOS ELIMINADOS, ESTO SI LO ESTA CONSIDERANDO...
             TotalRegisters = int((len(file.read()) - 4) / RECORD_SIZE)
 
-            print("Content: ", TotalRegisters)
+            # Obtenemos el nodo padre.
             pos = self.getRoot()
 
             isRoot = False
 
+            # Si el nodo padre es -1, significa que no hay ningun nodo en el arbol.
             if pos == -1:
                 file.seek(0, 0)
                 file.write(struct.pack("i", TotalRegisters - 1))
                 pos = TotalRegisters - 1
+
+                # Si es que estamos insertando un nodo padre.
                 isRoot = True
 
             NodePos = pos
+
+            # O(log2(n)), O(n) en el peor de los casos.
             while True:
 
+                # Los primeros 4 bytes es el nodo padre.
+
+                # Movemos el puntero al inicio del registro.
                 file.seek(4 + pos * RECORD_SIZE, 0)
 
-                # Devuelve un array con todos los campos puestos en EL FORMATO.
                 idNode = struct.unpack("i", file.read(4))[0]
 
                 if idNode > idInsert:
@@ -77,21 +90,22 @@ class BSTFile:
                     pos = struct.unpack("i", file.read(4))[0]
                     IsLeft = False
 
+                # Nodos sin left o right, se colocó -1.
                 if pos == -1:
                     break
                 else:
                     NodePos = pos
 
-            # Cambiando la referencia al nodo padre del registro insertado.
-
+            # Si es que el root existe:
             if TotalRegisters != 1 and not isRoot:
-
                 data = struct.pack("i", TotalRegisters - 1)
                 factor = 8 if IsLeft else 4
 
+                # Actualizamos el left o right del nodo que tiene como hijo al registro insertado.
                 file.seek(4 + (NodePos + 1) * RECORD_SIZE - factor, 0)
                 file.write(data)
 
+    # O(log2(n)), O(n) en el peor de los casos.
     def Search(self, ID):
 
         with open(self.FileName, "rb") as file:
@@ -101,22 +115,20 @@ class BSTFile:
 
             pos = self.getRoot()
 
+            # Si es que el archivo está vacio o el arbol está vacio.
+            # Realizado porque cuando insertemos el 1er elemento al arbol, el archivo binario estará vació.
             if TotalBytes == 0 or pos == -1:
                 return False
 
+            # O(log2(n)), O(n) en el peor de los casos.
             while True:
 
                 file.seek(4 + pos * RECORD_SIZE, 0)
                 idNode = struct.unpack("i", file.read(4))[0]
 
+                # Verificar si existe el registro.
                 if idNode == ID:
-
-                    # Si quiero retornar la venta, pero generalmente se quiere retornar el booleano.
-                    # Como avance 4 bytes, lo retrocedo en base la posición actual.
-                    # file.seek(-4, 1)
-                    # return Venta.to_data(file.read(RECORD_SIZE))
                     return True
-
 
                 # left
                 elif idNode > ID:
@@ -134,20 +146,33 @@ class BSTFile:
 
                     pos = struct.unpack("i", file.read(4))[0]
 
-                # Registro no existente.
+                # Si el arbol no se encuentra el registro, se llegará a un -1.
                 if pos == -1:
                     return False
 
+    # O(log2(n)) + O(log2(n)) + O(log2(n)), O(n) + O(n) + O(n) en el peor de los casos.
     def Remove(self, ID):
 
+        # Verificamos si el registro existe.
+        # O(log2(n)), O(n) en el peor de los casos.
         if self.Search(ID) is False:
             raise ValueError(f"El {ID} no existe en el registro.")
 
         with open(self.FileName, "r+b") as file:
 
             pos = self.getRoot()
+
+            # isRoot: necesario, porque si eliminamos el root, debemos actualizar los primeros 4 byts
+            # que permite al insert saber cuál es el punto de inicio del arbol.
             isRoot = True
+
+            # Necesario para saber si nos encontramos al nodo izquierdo o derecho del nodo padre.
             isLeft = None
+
+            # Lo que buscamos es rescatar el número de registro del nodo a eliminar y su padre
+            # para actualizar sus enlaces directamente desde el contenido del archivo binario.
+
+            # O(log2(n)), O(n) en el peor de los casos.
             while True:
 
                 file.seek(4 + pos * RECORD_SIZE, 0)
@@ -177,16 +202,21 @@ class BSTFile:
                     if isRoot:
                         isRoot = False
 
+            # Nos posicionamos en el nodo siguiente, para rescatar fácilmente
+            # los hijos del nodo a borrar.
             file.seek(4 + ((PosNode + 1) * RECORD_SIZE) - 8, 0)
 
             # hijos del nodo a borrar.
             left = struct.unpack("i", file.read(4))[0]
             right = struct.unpack("i", file.read(4))[0]
+
             factor = 8 if isLeft else 4
 
             # Caso 1: el nodo es hoja
             if left == -1 and right == -1:
 
+                # Si es root, solo debemos actualizar la referencia al nodo padre a -1
+                # lo cual indica, que el arbol está vació.
                 if isRoot:
                     file.seek(0, 0)
                     file.write(struct.pack("i", -1))
@@ -194,31 +224,42 @@ class BSTFile:
 
                     file.seek(0, 0)
                     data = struct.pack("i", -1)
+
+                    # Actualizamos al padre, ya no tiene como hijo al nodo a eliminar.
                     file.seek(4 + ((PosPadre + 1) * RECORD_SIZE) - factor, 0)
                     file.write(data)
 
             # Caso 2: el nodo tiene 1 hijo
+            # O(log2(n)), O(n) en el peor de los casos.
             elif (left != -1) ^ (right != -1):
 
+                # Verificamos si el nodo a eliminar tiene hijo izquierdo o derecho.
+                # factor: es con respecto al nodo padre, si el nodo a eliminar es left o right.
                 factorNode = 8 if left != -1 else 4
 
                 if isRoot:
+
+                    # Si eliminamos el root, el root no tiene PosPadre, su único hijo será el nuevo
+                    # root.
                     hijo = left if left != -1 else right
                     file.seek(0, 0)
+
+                    # Actualizamos al único hijo, como nuevo root.
                     file.write(struct.pack("i", hijo))
                     file.seek(4 + RECORD_SIZE - 8, 0)
 
-                    # borro ambos hijos. CHECAR
+                    # Elimino los enlaces del nodo padre.
                     file.write(struct.pack("i", -1))
                     file.write(struct.pack("i", -1))
                 else:
 
                     data = struct.pack("i", -1)
-                    # Actualizo el nodo a borrar para que no tenga enlaces.
+
+                    # Quitamos el enlace al nodo a eliminar.
                     file.seek(4 + (PosNode + 1) * RECORD_SIZE - factorNode, 0)
                     file.write(data)
 
-                    # hacer que el nodo padre apunte al hijo del nodo actual.
+                    # Le asignamos al nodo padre el nodo hijo del registro a eliminar.
                     swap = struct.pack("i", left if left != -1 else right)
                     file.seek(4 + (PosPadre + 1) * RECORD_SIZE - factor, 0)
                     file.write(swap)
@@ -226,73 +267,111 @@ class BSTFile:
             # Caso 3: el nodo tiene ambos hijos
             elif left != -1 and right != -1:
 
+                # Debemos actualizar:
+                # PosPadre: posición del nodo padre
+                # NodePos: posición del nodo a eliminar
+                # posIO: posición del nodo InOrder sucesor.
+                # PosPadreIO: posición del padre del nodo InOrder sucesor.
+
                 posIO = right
                 PosPadreIO = PosNode
+
+                # # Conseguiremos el sucesor con mínimo valor.
                 while True:
 
                     file.seek(4 + ((posIO + 1) * RECORD_SIZE) - 8, 0)
                     leftIO = struct.unpack("i", file.read(4))[0]
 
+                    # Indica que ya pasamos por un nodo hoja.
                     if leftIO == -1:
                         break
                     else:
                         PosPadreIO = posIO
                         posIO = leftIO
 
-                # Leer los enlaces del sucesor inorden
+                # Hijos del nodo candidato.
                 file.seek(4 + (posIO + 1) * RECORD_SIZE - 8, 0)
+
                 leftIO = struct.unpack("i", file.read(4))[0]
                 rightIO = struct.unpack("i", file.read(4))[0]
 
-                # Esto corresponde a actualizar los enlaces del SUCESOR, su padre del SUCESOR
-                # y del nodo a eliminar.
-
-                    # SACO LOS ENLACES DEL NODO A ELIMINAR
+                # EXTRAEMOS LOS ENLACES DEL NODO A ELIMINAR
                 file.seek(4 + (PosNode + 1) * RECORD_SIZE - 8, 0)
                 dataL = struct.pack("i", left)
 
-                # Con esto soluciono el problema cuando PosPadreIO == PosNode
+                # Si el nodo sucesor el hijo del nodo a eliminar, le anclaremos
+                # su right del sucesor, si no, el right del nodo a eliminar.
                 dataR = struct.pack("i", rightIO if PosPadreIO == PosNode else right)
 
-                    # LE QUITO LOS ENLACES AL NODO A ELIMINAR.
+                # LE QUITO LOS ENLACES AL NODO A ELIMINAR.
                 file.write(struct.pack("i", -1))
                 file.write(struct.pack("i", -1))
 
-                    # LOS COLOCO AL NODO SUCESOR.
+                # LOS COLOCO AL NODO SUCESOR.
                 file.seek(4 + (posIO + 1) * RECORD_SIZE - 8, 0)
                 file.write(dataL)
                 file.write(dataR)
 
-                # Actualizar la referencia del padre del sucesor inorden
-                # Si son iguales, con el cambio del sucesor InOrden es suficiente.
+                # Actualizar la referencia del padre del sucesor
                 if PosPadreIO != PosNode:
-
-                    # Si son diferentes, se asume que siempre será el hijo izquierdo
                     file.seek(4 + (PosPadreIO + 1) * RECORD_SIZE - 8, 0)
 
-                    # le pasamos el hijo derecho, en caso tenga. PERO LO PASAMOS COMO LEFT
+                    # Le pasamos el contenido del hijo derecho del nodo sucesor, ya que puede
+                    # pasar que el nodo candidato tenga right.
                     file.write(struct.pack("i", rightIO))
 
-
-                # Esto conlleva a actualizar el enlace del nodo padre al SUCESOR.
-
-                # Actualizamos el root.
+                # Si eliminamos el root, debemos actualizar la referencia al nuevo root, el cual sería
+                # el nodo candidato.
                 if isRoot:
                     file.seek(0, 0)
                     file.write(struct.pack("i", posIO))
                 else:
+
+                    # En caso no sea el root, debemos actualizar el enlace del nodo padre
+                    # al nodo candidato.
                     file.seek(4 + (PosPadre + 1) * RECORD_SIZE - factor, 0)
                     file.write(struct.pack("i", posIO))
 
+    # O(log2(n)) + O(log2(n)), O(n) + O(n) en el peor de los casos
+    def ReadVenta(self, ID):
 
-
-
-    def ReadVenta(self, pos):
+        # O(log2(n)), O(n) en el peor de los casos
+        if self.Search(ID) is False:
+            raise ValueError(f"El {ID} no existe en el registro.")
 
         with open(self.FileName, "rb") as file:
-            file.seek(4 + pos * RECORD_SIZE)
-            print(Venta.to_data(file.read(RECORD_SIZE)))
 
+            file.seek(0, 0)
+            pos = self.getRoot()
+
+            file.seek(0, 0)
+
+            # O(log2(n)), O(n) en el peor de los casos
+            while True:
+
+                file.seek(4 + pos * RECORD_SIZE, 0)
+                idNode = struct.unpack("i", file.read(4))[0]
+
+                if idNode == ID:
+                    break
+                elif idNode > ID:
+
+                    IdToLeft = RECORD_SIZE - 4 - 8
+                    file.seek(IdToLeft, 1)
+                    pos = struct.unpack("i", file.read(4))[0]
+
+                else:
+
+                    IdToRight = RECORD_SIZE - 4 - 4
+                    file.seek(IdToRight, 1)
+                    pos = struct.unpack("i", file.read(4))[0]
+
+            file.seek(0, 0)
+            file.seek(4 + pos * RECORD_SIZE)
+
+            return Venta.to_data(file.read(RECORD_SIZE))
+
+    # O(1), solo rescatamos los 4 primeros bytes, el root.
     def getRoot(self):
         with open(self.FileName, "rb") as file:
             file.seek(0, 0)
--- main.py
+++ main.py
@@ -1,45 +1,48 @@
 from BST.Venta import Venta
 from BST.BSTFile import BSTFile
 
-'''
-Venta(1, "Producto E", 15, 49.99, "2023-04-05"),
-Venta(4, "Producto C", 200, 9.99, "2023-04-03"),
-Venta(2, "Producto B", 50, 29.99, "2023-04-02"),
-Venta(7, "Producto G", 75, 25.00, "2023-04-07"),
-Venta(3, "Producto D", 0, 39.99, "2023-04-04"),
-Venta(6, "Producto F", 120, 15.50, "2023-04-06"),
-Venta(9, "Producto I", 150, 12.99, "2023-04-09"),
-Venta(8, "Producto H", 10, 89.99, "2023-04-08"),
-Venta(10, "Producto J", 30, 35.99, "2023-04-10"),
-'''
 
-if __name__ == "__main__":
-
+def Insert(bst):
     ventas = [
         Venta(2, "Producto B", 50, 29.99, "2023-04-02"),
+        Venta(1, "Producto E", 15, 49.99, "2023-04-05"),
+        Venta(4, "Producto C", 200, 9.99, "2023-04-03"),
         Venta(7, "Producto G", 75, 25.00, "2023-04-07"),
         Venta(3, "Producto D", 0, 39.99, "2023-04-04"),
         Venta(6, "Producto F", 120, 15.50, "2023-04-06"),
         Venta(9, "Producto I", 150, 12.99, "2023-04-09"),
         Venta(8, "Producto H", 10, 89.99, "2023-04-08"),
         Venta(10, "Producto J", 30, 35.99, "2023-04-10"),
-
     ]
 
-    # Muestra los detalles de las ventas
-    #for venta in ventas:
-    #    print(venta)
+    for venta in ventas:
+        bst.Insert(venta)
 
 
+
+def Search(bst):
+    print(bst.Search(20))
+    print(bst.Search(10))
+    print(bst.Search(5))
+    print(bst.Search(11))
+
+
+def Remove(bst):
+    bst.Remove(1)
+    bst.Remove(2)
+    bst.Remove(3)
+    bst.Remove(10)
+
+
+def ReadVenta(bst, ID):
+    venta = bst.ReadVenta(ID)
+
+    print(venta[0]())
+    print(f"Left: {venta[1]}")
+    print(f"Right: {venta[2]}")
+
+
+if __name__ == "__main__":
     bst = BSTFile("./BST/binario.dat")
 
-    #for venta in ventas:
-    #    bst.Insert(venta)
-
-    #bst.Remove(18)
-
-
-    # Falta testear desde EL ROOT para los 3 casos.
-    print(bst.ReadVenta(17))
-    print("Root: ", bst.getRoot())
-
+    ReadVenta(bst, 4)
