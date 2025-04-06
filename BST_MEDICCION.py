34357183ff2bfbd178c91bd301da068cd31e06a8
RODRIGO <rodrigo.meza@utec.edu.pe>
2025-04-05 22:22:26 -0500

    PUSH

--- BST_MEDICCION.py
+++ BST_MEDICCION.py
@@ -2,13 +2,16 @@ import os
 import struct
 import time
 import csv
+import numpy as np
 
 import matplotlib
+
 matplotlib.use("TkAgg")
 
 import matplotlib.pyplot as plt
-from AVL.AVLfile import AVLFile
+from BST.BSTFile import BSTFile
 from BST.Venta import Venta
+
 
 def cargar_ventas(csv_path):
     ventas = []
@@ -23,36 +26,41 @@ def cargar_ventas(csv_path):
                 fila["Fecha de venta"]
             )
             ventas.append(venta)
-    return ventas
 
-def benchmark_avl(filename, ventas):
+
+    np.random.shuffle(ventas)
+    return list(ventas)
+
+
+def benchmark_bst(filename, ventas):
     if os.path.exists(filename):
         os.remove(filename)
     with open(filename, "wb") as f:
         f.write(struct.pack("i", -1))  # raíz = -1
 
-    avl = AVLFile(filename)
+    bst = BSTFile(filename)
 
     tiempos = {}
 
     # 1. Inserción
     inicio = time.perf_counter()
     for v in ventas:
-        avl.insert(v)
+        bst.Insert(v)
     fin = time.perf_counter()
     tiempos["inserción"] = fin - inicio
 
     # 2. Búsqueda específica
-    ids_buscar = [ventas[0].id, ventas[len(ventas)//2].id, ventas[-1].id]
+    ids_buscar = [ventas[0].id, ventas[len(ventas) // 2].id, ventas[-1].id]
     inicio = time.perf_counter()
     for id in ids_buscar:
-        avl.search(id)
+        bst.Search(id)
+
     fin = time.perf_counter()
     tiempos["búsqueda específica"] = fin - inicio
 
     # 3. Búsqueda por rango
     inicio = time.perf_counter()
-    avl.search_range(ventas[0].id, ventas[-1].id)
+    bst.Search_Range(ventas[0].id, ventas[-1].id)
     fin = time.perf_counter()
     tiempos["búsqueda por rango"] = fin - inicio
 
@@ -60,13 +68,14 @@ def benchmark_avl(filename, ventas):
     ids_eliminar = [ventas[0].id, ventas[-1].id]
     inicio = time.perf_counter()
     for id in ids_eliminar:
-        avl.remove(id)
+        bst.Remove(id)
     fin = time.perf_counter()
     tiempos["eliminación"] = fin - inicio
 
-    #os.remove(filename)  # limpiar
+    # os.remove(filename)  # limpiar
 
     return tiempos
+
 
 def graficar_resultados(tiempos):
     operaciones = list(tiempos.keys())
@@ -75,16 +84,17 @@ def graficar_resultados(tiempos):
     plt.figure(figsize=(8, 5))
     plt.bar(operaciones, duraciones, color="skyblue")
     plt.ylabel("Tiempo (segundos)")
-    plt.title("Evaluación de Desempeño - AVL persistente")
+    plt.title("Evaluación de Desempeño - BST")
     plt.grid(axis="y")
     plt.tight_layout()
     plt.show()
 
+
 if __name__ == "__main__":
     csv_path = "sales_dataset.csv"
-    filename = "avl_perf.dat"
+    filename = "bst_peft.dat"
     ventas = cargar_ventas(csv_path)
-    tiempos = benchmark_avl(filename, ventas)
+    tiempos = benchmark_bst(filename, ventas)
     print("\n--- Resultados de Desempeño ---")
     for k, v in tiempos.items():
         print(f"{k}: {v:.6f} segundos")
